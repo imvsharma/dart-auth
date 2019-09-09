ca16e5c5e4790c1138f802846c78c96d1bc4b51d
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-09-09 21:17:01 +0530

    create user schema

---
+++ dart jwt auth/lib/model/user.model.dart
@@ -0,0 +1,27 @@
+
+
+import '../dart_jwt_auth.dart';
+
+class User extends ManagedObject<_User> implements _User{
+  
+}
+
+class _User {
+  @primaryKey
+  int id;
+
+  @Column(unique: true, indexed: true)
+  String username;
+
+  @Column()
+  String password;
+
+  @Column(unique: false, indexed: true)
+  String firstname;
+
+  @Column(unique: false, indexed: true)
+  String lastname;
+
+  @Column(unique: true, indexed: true)
+  String email;
+}
