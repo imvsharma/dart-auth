46530ca83f56a7a0ed67b3093d18635e3c46c4c3
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-09-10 22:19:51 +0530

    create signup user

---
+++ dart jwt auth/lib/controllers/signup.controller.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'package:aqueduct/aqueduct.dart';
+import '../model/user.model.dart';
+
+class SignupController extends ResourceController {
+  @Operation.post()
+  Future<Response> createUser(@Bind.body() User user) async {
+    print(user);
+    return Response.ok('user created');
+  }
+}
