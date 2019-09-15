16a6f9522d56db039083d154cff176ef2c16951f
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-09-15 11:04:36 +0530

    simple user create route created

--- dart jwt auth/lib/controllers/signup.controller.dart
+++ dart jwt auth/lib/controllers/signup.controller.dart
@@ -5,7 +5,7 @@ import '../model/user.model.dart';
 class SignupController extends ResourceController {
   @Operation.post()
   Future<Response> createUser(@Bind.body() User user) async {
-    print(user);
+    print(user.username);
     return Response.ok('user created');
   }
 }
--- dart jwt auth/lib/utils/hash.dart
+++ dart jwt auth/lib/utils/hash.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
 
+
 abstract class Hash {
   static Utf8Encoder encoder = const Utf8Encoder();
   static List<int> _convert(String x) => encoder.convert(x);
